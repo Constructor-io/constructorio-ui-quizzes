1bb24adf849b3085e89608f36618d369c4e1ae46
Jimmy Li <jimmyli921@gmail.com>
2023-01-25 11:11:53 -0800

    [CSL 2002] Consolidate answer inputs into single object (#9)
    * Consolidate answer inputs into single object
    
    * Adjust reducer for Cover type questions
    
    * Prettier fixes
    
    * Update AnswerInputState type
    
    * Updates based on comments & remove unused code
    
    * Update select type question component with new state

--- src/components/Quiz/actions.ts
+++ src/components/Quiz/actions.ts
@@ -12,16 +12,8 @@ export interface QuestionAnswer<Value> {
   input: Value;
 }
 
-export interface QuestionState<Value> {
-  [key: string]: Value;
-}
-
 export type SelectQuestionPayload = QuestionAnswer<string[]>;
-export type SelectQuestionState = QuestionState<string[]>;
-
 export type OpenTextQuestionPayload = QuestionAnswer<string>;
-export type OpenTextQuestionState = QuestionState<string>;
-
 export type ShowResultsPayload = string;
 
 interface Action<Type, Payload = {}> {
@@ -29,10 +21,13 @@ interface Action<Type, Payload = {}> {
   payload?: Payload;
 }
 
-export type ActionAnswerQuestion =
-  | Action<QuestionTypes.Cover>
+export type ActionAnswerInputQuestion =
   | Action<QuestionTypes.OpenText, OpenTextQuestionPayload>
   | Action<QuestionTypes.SingleSelect, SelectQuestionPayload>
-  | Action<QuestionTypes.MultipleSelect, SelectQuestionPayload>
+  | Action<QuestionTypes.MultipleSelect, SelectQuestionPayload>;
+
+export type ActionAnswerQuestion =
+  | ActionAnswerInputQuestion
+  | Action<QuestionTypes.Cover>
   | Action<QuestionTypes.Back, boolean>
   | Action<QuestionTypes.Reset>;
