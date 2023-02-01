ba26a19b4a460357c66a0682c12bce0fa95e34dc
mocca102 <islaam.agamy@gmail.com>
2023-02-01 22:37:23 +0200

    add console error if neither apiKey nor cioClient provided

--- src/hooks/useCioClient.ts
+++ src/hooks/useCioClient.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import ConstructorIOClient from '@constructor-io/constructorio-client-javascript';
 import { useMemo } from 'react';
 import { getCioClient } from '../utils';
@@ -5,7 +6,12 @@ import { getCioClient } from '../utils';
 export type CioClientConfig = { apiKey?: string; cioJsClient?: ConstructorIOClient };
 type UseCioClient = (cioClientConfig: CioClientConfig) => ConstructorIOClient | null;
 
-const useCioClient: UseCioClient = ({ apiKey, cioJsClient }) =>
-  useMemo(() => cioJsClient || getCioClient(apiKey), [apiKey, cioJsClient]);
+const useCioClient: UseCioClient = ({ apiKey, cioJsClient }) => {
+  if (!apiKey && !cioJsClient) {
+    console.error('Either apiKey or cioJsClient is required');
+  }
+
+  return useMemo(() => cioJsClient || getCioClient(apiKey), [apiKey, cioJsClient]);
+};
 
 export default useCioClient;
