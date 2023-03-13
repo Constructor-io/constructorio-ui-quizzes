d1171f24794e615551537c942c43b93cb882f167
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2023-03-13 15:48:16 -0400

    [CSL-2133] Default Theme Cover Page Component (#30)
    * Mobile
    
    * Tablet and some image logic
    
    * Cleanup and organization
    
    * Back button
    
    * Use primary-color
    
    * Update back button
    
    * Add left margin to button
    
    * Add hover styles
    
    * Update border
    
    * Update breakpoints

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -2,6 +2,7 @@ import { Title, Description, Stories } from '@storybook/addon-docs';
 import './custom-styles-story.css';
 import './storybook-styles.css';
 import '../src/styles.css'
+import '../src/new-styles.css'
 
 export const parameters = {
   actions: { argTypesRegex: '^on.*' },
--- src/components/CoverTypeQuestion/CoverTypeQuestion.tsx
+++ src/components/CoverTypeQuestion/CoverTypeQuestion.tsx
@@ -3,6 +3,7 @@ import QuestionTitle from '../QuestionTitle/QuestionTitle';
 import QuizContext from '../CioQuiz/context';
 import QuestionDescription from '../QuestionDescription/QuestionDescription';
 import CTAButton from '../CTAButton/CTAButton';
+import BackButton from '../BackButton/BackButton';
 import { renderImages } from '../../utils';
 import { QuestionTypes } from '../CioQuiz/actions';
 
@@ -10,9 +11,12 @@ export default function CoverTypeQuestion() {
   const { questionResponse, quizBackHandler, quizNextHandler, isFirstQuestion } =
     useContext(QuizContext);
   let question;
+
   if (questionResponse) {
     question = questionResponse.next_question;
   }
+
+  const hasImage = question?.images?.primary_url;
 
   const onNextClick = () => {
     if (quizNextHandler) {
@@ -27,16 +31,24 @@ export default function CoverTypeQuestion() {
 
   if (question) {
     return (
-      <div className='cio-cover-question-container'>
-        <div className='cio-cover-question-text'>
-          <QuestionTitle title={question?.title} />
-          <QuestionDescription description={question.description} />
-          <CTAButton ctaText={question?.cta_text} onClick={onNextClick} />
-          {!isFirstQuestion && <CTAButton ctaText='Back' onClick={quizBackHandler} />}
+      <div className={`cio-cover-question-container ${hasImage ? 'with-image' : ''}`}>
+        <div className='cio-cover-question-text-container'>
+          <div className='cio-cover-question-text'>
+            <div>
+              <QuestionTitle title={question?.title} />
+              <QuestionDescription description={question.description} />
+            </div>
+            <div className='cio-cover-question-buttons'>
+              {!isFirstQuestion && <BackButton onClick={quizBackHandler} />}
+              <CTAButton ctaText={question?.cta_text} onClick={onNextClick} />
+            </div>
+          </div>
         </div>
-        <div className='cio-cover-question-img'>
-          {question?.images ? renderImages(question.images) : ''}
-        </div>
+        {hasImage ? (
+          <div className='cio-cover-question-img'>{renderImages(question.images)}</div>
+        ) : (
+          ''
+        )}
       </div>
     );
   }
