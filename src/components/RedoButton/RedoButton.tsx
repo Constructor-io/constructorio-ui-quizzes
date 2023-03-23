ac4b402570e503f166ad45faa4aeb7a80093700e
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2023-03-23 17:11:47 -0400

    Basic layout

---
+++ src/components/RedoButton/RedoButton.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+interface RedoButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  redoText?: string;
+}
+
+function RedoButton(props: RedoButtonProps) {
+  const { redoText = 'Redo Quiz', disabled, ...rest } = props;
+
+  return (
+    <button
+      type='button'
+      className={`${disabled ? 'cio-question-redo-button disabled' : 'cio-question-redo-button'}`}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...rest}>
+      <svg
+        width='16'
+        height='16'
+        viewBox='0 0 16 16'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'>
+        <g clipPath='url(#clip0_7495_177230)'>
+          <path
+            d='M0.667969 2.66699V6.66699H4.66797'
+            stroke='#0A0B0C'
+            strokeWidth='1.33333'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+          <path
+            d='M15.332 13.333V9.33301H11.332'
+            stroke='#0A0B0C'
+            strokeWidth='1.33333'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+          <path
+            d='M13.6613 6.00038C13.3232 5.0449 12.7485 4.19064 11.991 3.51732C11.2334 2.844 10.3177 2.37355 9.32911 2.14988C8.34056 1.92621 7.31147 1.9566 6.33784 2.23823C5.36422 2.51985 4.4778 3.04352 3.7613 3.76038L0.667969 6.66704M15.3346 9.33371L12.2413 12.2404C11.5248 12.9572 10.6384 13.4809 9.66476 13.7625C8.69114 14.0441 7.66204 14.0745 6.67349 13.8509C5.68495 13.6272 4.76917 13.1568 4.01161 12.4834C3.25406 11.8101 2.67941 10.9559 2.3413 10.0004'
+            stroke='#0A0B0C'
+            strokeWidth='1.33333'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+        </g>
+        <defs>
+          <clipPath id='clip0_7495_177230'>
+            <rect width='16' height='16' fill='white' />
+          </clipPath>
+        </defs>
+      </svg>
+      <span>{redoText}</span>
+    </button>
+  );
+}
+
+export default RedoButton;
--- src/components/ResultContainer/ResultContainer.tsx
+++ src/components/ResultContainer/ResultContainer.tsx
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react';
-import CTAButton from '../CTAButton/CTAButton';
+import RedoButton from '../RedoButton/RedoButton';
 import { QuestionTypes } from '../CioQuiz/actions';
 import QuizContext from '../CioQuiz/context';
 import ResultCard from '../ResultCard/ResultCard';
-import ResultHeroCard from '../ResultHeroCard/ResultHeroCard';
 
 interface ResultContainerProps {
   numResults?: number;
 }
 
 export default function ResultContainer(props: ResultContainerProps) {
-  const { numResults = 6 } = props;
+  // Params:
+  // Results to display
+  const { numResults = 10 } = props;
   const { resultsResponse } = useContext(QuizContext);
   const { dispatch } = useContext(QuizContext);
 
@@ -23,18 +24,23 @@ export default function ResultContainer(props: ResultContainerProps) {
   };
 
   if (resultsResponse) {
-    const results = resultsResponse.response?.results;
     return (
-      <div className='cio-result-container'>
-        <h1 className='cio-result-container-text'>Here is your results</h1>
-
-        {results && results.length > 0 ? <ResultHeroCard heroItem={results[0]} /> : ''}
+      <div className='cio-results-container'>
+        <h1 className='cio-results-title'>Here are your results</h1>
+        <div className='cio-results-filter-container'>
+          <p>Because you answered</p>
+          <div className='cio-results-filter-options'>
+            <div className='cio-results-filter-option'>Option</div>
+            <div className='cio-results-filter-option'>Option</div>
+            <div className='cio-results-filter-option'>Option</div>
+          </div>
+        </div>
+        <RedoButton onClick={onResetClick} />
         <div className='cio-results'>
-          {resultsResponse?.response?.results?.slice(1, numResults).map((result) => (
+          {resultsResponse?.response?.results?.slice(0, numResults).map((result) => (
             <ResultCard result={result} key={result.data?.id} />
           ))}
         </div>
-        <CTAButton ctaText='Reset' onClick={onResetClick} />
       </div>
     );
   }
