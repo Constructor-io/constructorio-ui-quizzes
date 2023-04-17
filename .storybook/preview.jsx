1ed1e272db38d3d935d9ff1c3b550092626f4d94
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2023-04-17 16:32:40 -0400

    Update Storybook to use new styles

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -3,7 +3,6 @@ import { Title, Description, Stories } from '@storybook/addon-docs';
 import './custom-styles-story.css';
 import './storybook-styles.css';
 import '../src/styles.css'
-import '../src/new-styles.css'
 
 export const parameters = {
   actions: { argTypesRegex: '^on.*' },
