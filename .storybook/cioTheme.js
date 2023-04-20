804d37743528c83715fe179172290e62ac91600c
Steve Blaurock <sblaurock@users.noreply.github.com>
2023-04-20 01:02:13 -0600

    Update Storybook brand URL. (#54)

--- .storybook/cioTheme.js
+++ .storybook/cioTheme.js
@@ -3,7 +3,7 @@ import { getPreferredColorScheme } from '../src/utils'
 
 export default create({
   brandTitle: 'Constructor.io',
-  brandUrl: 'https://github.com/Constructor-io/constructorio-ui-autocomplete',
+  brandUrl: 'https://github.com/Constructor-io/constructorio-ui-quizzes',
   brandImage:  getPreferredColorScheme() === 'light' ? 'https://docs.constructor.io/img/logo-dark.svg' : 'https://docs.constructor.io/img/logo-light.svg',
   brandTarget: '_blank',
 });
