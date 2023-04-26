2642f814f31bddbccb995b6f9071341e64bc9d59
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2023-04-26 14:28:08 -0400

    Add additional parameters

--- src/bundled.jsx
+++ src/bundled.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CioQuiz from './components/CioQuiz';
-import './new-styles.css';
+import './styles.css';
 
-export default ({ containerElement, apiKey, quizId }) => {
-  ReactDOM.createRoot(containerElement).render(
-    <React.StrictMode>
-      <CioQuiz quizId={quizId} apiKey={apiKey} />;
-    </React.StrictMode>
-  );
+const ConstructorioQuizzes = ({
+  selector,
+  apiKey,
+  quizId,
+  cioJsClient,
+  resultsPageOptions,
+  quizVersionId,
+}) => {
+  if (document) {
+    const containerElement = document.querySelector(selector);
+
+    ReactDOM.createRoot(containerElement).render(
+      <React.StrictMode>
+        <CioQuiz
+          quizId={quizId}
+          apiKey={apiKey}
+          cioJsClient={cioJsClient}
+          resultsPageOptions={resultsPageOptions}
+          quizVersionId={quizVersionId}
+        />
+        ;
+      </React.StrictMode>
+    );
+  }
 };
+
+if (window) {
+  window.ConstructorioQuizzes = ConstructorioQuizzes;
+}
+
+export default ConstructorioQuizzes;
