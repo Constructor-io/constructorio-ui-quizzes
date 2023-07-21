1c0e70459db2ea7b8ef453b0c244de6e9a65ff69
Vlad Saitov <vladislav.saitov@constructor.io>
2023-07-21 16:35:05 +0500

    UI Library: No Results Experience

--- src/components/ZeroResults/ZeroResults.tsx
+++ src/components/ZeroResults/ZeroResults.tsx
@@ -11,13 +11,9 @@ function ZeroResults(props: ZeroResultsProps) {
   return (
     <div className='cio-zero-results'>
       <h3 className='cio-zero-results-subtitle'>
-        Sorry, it seems like we couldn’t find results based on your answers.
+        Sorry, we couldn’t find products that perfectly match your preferences.
       </h3>
-      <p className='cio-zero-results-description'>
-        This is embarrassing 😢. It might be that some of the questions are not properly set up from
-        our end. Would you give us another try?
-      </p>
-      <CTAButton ctaText='Try Again' onClick={resetQuizClickHandler} />
+      <CTAButton ctaText='Redo quiz' onClick={resetQuizClickHandler} />
     </div>
   );
 }
