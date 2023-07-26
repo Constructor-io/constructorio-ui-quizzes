5a9a0990675e051850f640a8f02cda0bb0cc9840
HHHindawy <7ossam9063@gmail.com>
2023-07-26 18:24:49 +0300

    Fix storybook tests

--- src/stories/Quiz/tests/mocks.ts
+++ src/stories/Quiz/tests/mocks.ts
@@ -80,6 +80,7 @@ export const getMockState = (question?: Question): QuizReturnState => ({
     requestState: RequestStates.Success,
     versionId: '',
     sessionId: '',
+    isCompleted: false,
     currentQuestion: {
       next_question: question!,
       isFirstQuestion: false,
