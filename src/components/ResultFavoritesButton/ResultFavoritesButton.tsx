23b7fa878bdd95dd166035e4863372006bf4c585
Stanley Peng <speng1011@gmail.com>
2023-08-03 10:02:29 -0700

    file rename

--- src/components/ResultFavoritesButton/ResultFavoritesButton.tsx
+++ src/components/ResultFavoritesButton/ResultFavoritesButton.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { QuizResultDataPartial } from '../../types';
 import QuizContext from '../CioQuiz/context';
 
