92c8550630473e3e71b25fa6df33c21e0f6a8ede
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2023-08-24 14:46:45 -0400

    Implementation

--- src/hooks/useQuizEvents/useQuizNextClick.ts
+++ src/hooks/useQuizEvents/useQuizNextClick.ts
@@ -2,26 +2,39 @@ import { useCallback } from 'react';
 import { ActionAnswerQuestion, QuestionTypes } from '../../components/CioQuiz/actions';
 import { QuizAPIReducerState } from '../../components/CioQuiz/quizApiReducer';
 import { QuizLocalReducerState } from '../../components/CioQuiz/quizLocalReducer';
-import { QuizEventsReturn } from '../../types';
+import { QuizEventsReturn, OnQuizNextQuestion } from '../../types';
+import { isFunction } from '../../utils';
 
 const useQuizNextClick = (
   quizApiState: QuizAPIReducerState,
   quizLocalState: QuizLocalReducerState,
-  dispatchLocalState: React.Dispatch<ActionAnswerQuestion>
+  dispatchLocalState: React.Dispatch<ActionAnswerQuestion>,
+  onQuizNextQuestion?: OnQuizNextQuestion
 ): QuizEventsReturn.NextQuestion => {
   const quizNexClickHandler = useCallback(() => {
     const currentQuestion = quizApiState.quizCurrentQuestion?.next_question;
     const currentQuestionId = currentQuestion?.id;
+
     if (dispatchLocalState && currentQuestionId) {
       const currentAnswerInput = quizLocalState.answerInputs[currentQuestionId];
+
       if (currentAnswerInput?.value?.length || currentQuestion?.type === 'cover') {
         dispatchLocalState({
           type: QuestionTypes.Next,
           payload: quizApiState.quizCurrentQuestion,
         });
       }
+
+      if (currentQuestion && onQuizNextQuestion && isFunction(onQuizNextQuestion)) {
+        onQuizNextQuestion({ ...currentQuestion, answer: currentAnswerInput });
+      }
     }
-  }, [dispatchLocalState, quizApiState.quizCurrentQuestion, quizLocalState.answerInputs]);
+  }, [
+    dispatchLocalState,
+    quizApiState.quizCurrentQuestion,
+    quizLocalState.answerInputs,
+    onQuizNextQuestion,
+  ]);
 
   return quizNexClickHandler;
 };
