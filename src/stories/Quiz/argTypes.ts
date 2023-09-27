dfc59a5cf04531aa55b584869ced02d21616fe7c
Jimmy Li <jimmyli921@gmail.com>
2023-09-27 14:09:32 -0700

    testing some stuff

--- src/stories/Quiz/argTypes.ts
+++ src/stories/Quiz/argTypes.ts
@@ -12,15 +12,75 @@ export const argTypes = {
   apiKey: {
     description: 'Your Constructor API key. Either `apiKey` or `cioJsClient` are required',
   },
-  callbacks: {
-    description: `Callback functions to be called when events are fired
-    \`onQuizNextQuestion?: (question) => void);\`
-    \`onQuizResultsLoaded?: (results) => void;\`
-    \`onQuizResultClick?: (result, position: number) => void;\`
-    \`onAddToCartClick?: (result) => void;\`
-    \`onAddToFavoritesClick?: (result) => void;\`
-    `,
+  onQuizNextQuestion: {
+    description: 'Callback function to be called when the next question is loaded',
     control: false,
+    table: {
+      subcategory: 'callbacks',
+      defaultValue: {
+        summary: 'null',
+      },
+      type: {
+        summary: '(question) => void',
+        detail: '(question: QuestionWithAnswer) => void',
+      },
+    },
+  },
+  onQuizResultsLoaded: {
+    description: 'Callback function to be called when the quiz results are loaded',
+    control: false,
+    table: {
+      subcategory: 'callbacks',
+      defaultValue: {
+        summary: 'null',
+      },
+      type: {
+        summary: '(results) => void',
+        detail: '(results: QuizResultDataPartial) => void',
+      },
+    },
+  },
+  onQuizResultClick: {
+    description: 'Callback function to be called when a quiz result is clicked',
+    control: false,
+    table: {
+      subcategory: 'callbacks',
+      defaultValue: {
+        summary: 'null',
+      },
+      type: {
+        summary: '(result, position: number) => void',
+        detail: '(result: QuizResultDataPartial, position: number) => void',
+      },
+    },
+  },
+  onAddToCartClick: {
+    description: 'Callback function to be called when the add to cart button is clicked',
+    control: false,
+    table: {
+      subcategory: 'callbacks',
+      defaultValue: {
+        summary: 'null',
+      },
+      type: {
+        summary: '(result) => void',
+        detail: '(result: QuizResultDataPartial) => void',
+      },
+    },
+  },
+  onAddToFavoritesClick: {
+    description: 'Callback function to be called when the add to favorites button is clicked',
+    control: false,
+    table: {
+      subcategory: 'callbacks',
+      defaultValue: {
+        summary: 'null',
+      },
+      type: {
+        summary: '(result) => void',
+        detail: '(result: QuizResultDataPartial) => void',
+      },
+    },
   },
   cioJsClient: {
     description:
@@ -42,15 +102,140 @@ export const argTypes = {
      \n And \`renderResultCardPriceDetails: (result) => JSX.Element\` a render result card price details callback function
     `,
   },
-  resultsPageOptions: {
-    description: 'Results page configuration options `numResultsToDisplay: number`',
+  resultCardRegularPriceKey: {
+    description: 'Key name for the regular price in the API response',
+    control: {
+      type: 'text',
+    },
+    table: {
+      subcategory: 'resultCardOptions',
+      defaultValue: {
+        summary: 'regular_price',
+      },
+      type: {
+        summary: 'string',
+      },
+    },
   },
-  sessionStateOptions: {
-    description: `Session Storage state configuration options
-    \`showSessionModal?: boolean;\` (Whether to show modal to hydrate quiz on quiz results page)
-    \`showSessionModalOnResults?: boolean;\`(Whether to show modal to hydrate quiz)
-    \`sessionStateKey?: string;\`(key name where session storage state is saved)
-    `,
+  resultCardSalePriceKey: {
+    description: 'Key name for the sale price in the API response',
+    control: {
+      type: 'text',
+    },
+    table: {
+      subcategory: 'resultCardOptions',
+      defaultValue: {
+        summary: 'sale_price',
+      },
+      type: {
+        summary: 'string',
+      },
+    },
+  },
+  resultCardRatingCountKey: {
+    description: 'Key name for the rating count in the API response',
+    control: {
+      type: 'text',
+    },
+    table: {
+      subcategory: 'resultCardOptions',
+      defaultValue: {
+        summary: 'rating_count',
+      },
+      type: {
+        summary: 'string',
+      },
+    },
+  },
+  resultCardRatingScoreKey: {
+    description: 'Key name for the rating score in the API response',
+    control: {
+      type: 'text',
+    },
+    table: {
+      subcategory: 'resultCardOptions',
+      defaultValue: {
+        summary: 'rating_score',
+      },
+      type: {
+        summary: 'string',
+      },
+    },
+  },
+  renderResultCardPriceDetails: {
+    description: 'Callback function to render result card price details',
+    control: false,
+    table: {
+      subcategory: 'resultCardOptions',
+      defaultValue: {
+        summary: 'null',
+      },
+      type: {
+        summary: '(result) => JSX.Element',
+        detail: '(result: QuizResultDataPartial) => JSX.Element',
+      },
+    },
+  },
+  numResultsToDisplay: {
+    description: 'Number of results to display on the results page',
+    control: {
+      type: 'number',
+    },
+    table: {
+      subcategory: 'resultsPageOptions',
+      defaultValue: {
+        summary: 5,
+      },
+      type: {
+        summary: 'number',
+      },
+    },
+  },
+  showSessionModal: {
+    description:
+      'Boolean for whether or not to show session modal to hydrate quiz on the results page',
+    control: {
+      type: 'boolean',
+    },
+    table: {
+      subcategory: 'sessionStateOptions',
+      defaultValue: {
+        summary: false,
+      },
+      type: {
+        summary: 'boolean',
+      },
+    },
+  },
+  showSessionModalOnResults: {
+    description: 'Boolean for whether or not to show session modal to hydrate quiz',
+    control: {
+      type: 'boolean',
+    },
+    table: {
+      subcategory: 'sessionStateOptions',
+      defaultValue: {
+        summary: false,
+      },
+      type: {
+        summary: 'boolean',
+      },
+    },
+  },
+  sessionStateKey: {
+    description: 'Key name where session storage state is saved',
+    control: {
+      type: 'text',
+    },
+    table: {
+      subcategory: 'sessionStateOptions',
+      defaultValue: {
+        summary: 'quizState',
+      },
+      type: {
+        summary: 'string',
+      },
+    },
   },
   enableHydration: {
     description: 'Boolean for whether or not to hydrate quiz questions and results on page reload',
