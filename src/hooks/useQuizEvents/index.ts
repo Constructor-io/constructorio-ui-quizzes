7267a7c62fe5ef129e83c2da1360849c6fbb9005
mocca102 <islaam.agamy@gmail.com>
2023-10-03 23:43:29 +0300

    Destructure callbacks

--- src/hooks/useQuizEvents/index.ts
+++ src/hooks/useQuizEvents/index.ts
@@ -28,6 +28,14 @@ const useQuizEvents: UseQuizEvents = (quizOptions, cioClient, quizState) => {
     quizSessionStorageState,
   } = quizState;
   const { callbacks } = quizOptions;
+  const {
+    onAddToCartClick,
+    onAddToFavoritesClick,
+    onQuizNextQuestion,
+    onQuizResultClick,
+    onQuizResultsLoaded,
+    onQuizSkipQuestion,
+  } = callbacks || {};
 
   // Quiz answer change
   const quizAnswerChanged = useQuizAnswerChangeHandler(quizApiState, dispatchLocalState);
@@ -37,33 +45,29 @@ const useQuizEvents: UseQuizEvents = (quizOptions, cioClient, quizState) => {
     quizApiState,
     quizLocalState,
     dispatchLocalState,
-    callbacks?.onQuizNextQuestion
+    onQuizNextQuestion
   );
 
   const skipQuestion = useQuizSkipClick(
     quizApiState,
     quizLocalState,
     dispatchLocalState,
-    callbacks?.onQuizSkipQuestion
+    onQuizSkipQuestion
   );
 
   // Quiz Back button click callback
   const previousQuestion = useQuizBackClick(quizApiState, dispatchLocalState);
 
   // Quiz result add to cart callback
-  const addToCart = useQuizAddToCart(cioClient, quizApiState, callbacks?.onAddToCartClick);
+  const addToCart = useQuizAddToCart(cioClient, quizApiState, onAddToCartClick);
 
   // Quiz result click callback
-  const resultClick = useQuizResultClick(cioClient, quizApiState, callbacks?.onQuizResultClick);
+  const resultClick = useQuizResultClick(cioClient, quizApiState, onQuizResultClick);
 
-  const addToFavorites = useQuizAddToFavorites(
-    cioClient,
-    quizApiState,
-    callbacks?.onAddToFavoritesClick
-  );
+  const addToFavorites = useQuizAddToFavorites(cioClient, quizApiState, onAddToFavoritesClick);
 
   // Quiz results loaded event
-  useQuizResultsLoaded(cioClient, quizApiState, callbacks?.onQuizResultsLoaded);
+  useQuizResultsLoaded(cioClient, quizApiState, onQuizResultsLoaded);
 
   // Quiz reset
   const resetQuiz = useQuizResetClick(
