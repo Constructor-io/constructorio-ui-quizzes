6d5e9380d0a2e6acf16fbf00e018eb3a717d1b3e
lws803 <lws803@gmail.com>
2023-10-18 12:28:26 -0400

    added more styling to share modal

--- src/components/ShareResultsModal/ShareResultsModal.tsx
+++ src/components/ShareResultsModal/ShareResultsModal.tsx
@@ -1,19 +1,42 @@
 import React from 'react';
 
+import CloseSVG from './CloseSVG';
+
 import { QuizResultsResponse } from '../../types';
 
 export default function ShareResultsModal({ showShareModal, setShowShareModal, results }: Props) {
-  console.log('🚀 ~ file: ShareResultsModal.tsx:7 ~ ShareResultsModal ~ state:', results);
-
   if (!showShareModal) return null;
 
   return (
-    <div className='cio-share-results-modal' role='presentation'>
+    <div
+      className='cio-share-results-modal'
+      role='presentation'
+      onClick={() => setShowShareModal(false)}>
       <div className='cio-share-results-container'>
         <div className='cio-share-results-content' role='presentation'>
-          <button onClick={() => setShowShareModal(false)} type='button'>
-            close
-          </button>
+          <div className='cio-share-results-heading'>
+            <div className='cio-share-results-title'>Share results</div>
+            <button
+              onClick={() => setShowShareModal(false)}
+              type='button'
+              className='cio-modal-close-button'>
+              <CloseSVG />
+            </button>
+          </div>
+          <div>Share or save your quiz results with this link.</div>
+          <div className='cio-share-results-button-group'>
+            <input
+              className='cio-share-results-input'
+              value='http://www.website.com/quiz/results/121323442'
+              disabled
+            />
+            <button
+              className='cio-share-results-share-button'
+              type='button'
+              onClick={() => console.log(results)}>
+              Copy link
+            </button>
+          </div>
         </div>
       </div>
     </div>
