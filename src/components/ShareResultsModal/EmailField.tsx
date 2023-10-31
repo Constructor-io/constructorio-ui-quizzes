85a550b81a9df26541cac20a5d7d5af28d154ad3
lws803 <lws803@gmail.com>
2023-10-31 11:20:44 -0400

    added error state

--- src/components/ShareResultsModal/EmailField.tsx
+++ src/components/ShareResultsModal/EmailField.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 
 import CheckMarkCircleSVG from './CheckMarkCircleSVG';
+import AlertCircleSVG from './AlertCircleSVG';
 
 interface EmailFieldProps {
   onSubmit: (email: string) => Promise<void>;
@@ -12,11 +13,22 @@ export default function EmailField({ onSubmit }: EmailFieldProps) {
     register,
     handleSubmit,
     formState: { errors, isSubmitSuccessful },
+    setError,
   } = useForm<{ email: string }>();
 
   return (
     <div className='cio-share-results-feature-group'>
-      <form onSubmit={handleSubmit(({ email }) => onSubmit(email))}>
+      <form
+        onSubmit={handleSubmit(async ({ email }) => {
+          try {
+            await onSubmit(email);
+          } catch (_e) {
+            setError('email', {
+              type: 'callback',
+              message: 'Sorry, there was an error sending. Please try again.',
+            });
+          }
+        })}>
         <div className='cio-share-results-description'>Share by email</div>
         <div className='cio-share-results-button-group'>
           <div className='cio-share-results-email-input-group'>
@@ -31,7 +43,7 @@ export default function EmailField({ onSubmit }: EmailFieldProps) {
                 },
               })}
             />
-            {errors.email && (
+            {errors.email?.type === 'validate' && (
               <div className='cio-share-results-email-input-error-message'>
                 {errors.email.message}
               </div>
@@ -43,9 +55,15 @@ export default function EmailField({ onSubmit }: EmailFieldProps) {
         </div>
       </form>
       {isSubmitSuccessful && (
-        <div className='cio-share-results-copied-notification'>
+        <div className='cio-share-results-notification'>
           <CheckMarkCircleSVG />
           <div>Email sent</div>
+        </div>
+      )}
+      {errors.email?.type === 'callback' && (
+        <div className='cio-share-results-notification'>
+          <AlertCircleSVG />
+          <div>{errors.email.message}</div>
         </div>
       )}
     </div>
--- src/components/ShareResultsModal/LinkField.tsx
+++ src/components/ShareResultsModal/LinkField.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import CheckMarkCircleSVG from './CheckMarkCircleSVG';
+import AlertCircleSVG from './AlertCircleSVG';
 
 interface LinkFieldProps {
   url: string;
@@ -8,6 +9,7 @@ interface LinkFieldProps {
 
 export default function LinkField({ url }: LinkFieldProps) {
   const [isCopied, setIsCopied] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   return (
     <div className='cio-share-results-feature-group'>
@@ -18,16 +20,26 @@ export default function LinkField({ url }: LinkFieldProps) {
           className='cio-share-results-share-button'
           type='button'
           onClick={() => {
-            navigator.clipboard.writeText(url);
-            setIsCopied(true);
+            try {
+              navigator.clipboard.writeText(url);
+              setIsCopied(true);
+            } catch (_e) {
+              setIsError(true);
+            }
           }}>
           Copy link
         </button>
       </div>
       {isCopied && (
-        <div className='cio-share-results-copied-notification'>
+        <div className='cio-share-results-notification'>
           <CheckMarkCircleSVG />
           <div>Link copied to clipboard</div>
+        </div>
+      )}
+      {isError && (
+        <div className='cio-share-results-notification'>
+          <AlertCircleSVG />
+          <div>Sorry, there was an error copying. Please try again.</div>
         </div>
       )}
     </div>
