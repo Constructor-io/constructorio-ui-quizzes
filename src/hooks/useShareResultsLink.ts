c0910f2109915a8580d97208ba1481dfa6ac7a5a
lws803 <lws803@gmail.com>
2023-11-02 14:25:08 -0400

    throw error if results don't exist

--- src/hooks/useShareResultsLink.ts
+++ src/hooks/useShareResultsLink.ts
@@ -3,9 +3,14 @@ import { QuizReturnState } from '../types';
 export default function useShareResultsLink(quizState: QuizReturnState['quiz']) {
   const urlObj = new URL(window.location.href);
   const existingParams = urlObj.searchParams;
+
+  if (!quizState.results?.response?.results) {
+    throw new Error("Can't generate share link without results");
+  }
+
   existingParams.set(
     'items',
-    quizState.results?.response?.results
+    quizState.results.response.results
       .filter((item) => item.data?.id)
       .map((item) => item.data!.id)
       .join(',') || ''
@@ -14,6 +19,7 @@ export default function useShareResultsLink(quizState: QuizReturnState['quiz']) 
     'attributes',
     quizState.selectedOptionsWithAttributes?.map((option) => option).join(',') || ''
   );
+
   const value = urlObj.toString();
 
   return value;
