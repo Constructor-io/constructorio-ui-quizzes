a16a6cd63187f516ac25807c11ffeb74400fcd9b
vishnu <vishnu452@gmail.com>
2024-03-06 21:07:28 +0530

    husky pre commit, set up react testing library env

--- src/components/SkipButton/SkipButton.test.tsx
+++ src/components/SkipButton/SkipButton.test.tsx
@@ -6,20 +6,24 @@ import SkipButton from './SkipButton';
 describe(SkipButton, () => {
   const props: React.ComponentProps<typeof SkipButton> = {
     propsGetters: () => ({}),
+    skipQuestionButtonText: 'Custom Button Text',
+    className: 'custom-class',
   };
 
   it('renders a button with the default properties', () => {
-    const { container } = render(<SkipButton {...props} />);
+    const { container } = render(<SkipButton propsGetters={() => ({})} />);
     expect(screen.getByText('Skip')).toBeInTheDocument();
     expect(container.firstChild).toHaveClass('cio-button-container');
   });
 
   it('renders a button with custom properties', () => {
-    const { container } = render(
-      <SkipButton {...props} skipQuestionButtonText='Custom Button Text' className='custom-class' />
-    );
+    const { container } = render(<SkipButton {...props} />);
     expect(screen.getByText('Custom Button Text')).toBeInTheDocument();
-    expect(screen.queryByText('Skip')).not.toBeInTheDocument();
     expect(container.firstChild).toHaveClass('custom-class');
+  });
+
+  it('does not render button without propsGetters', () => {
+    render(<SkipButton />);
+    expect(screen.queryByRole('button', { name: 'Skip' })).not.toBeInTheDocument();
   });
 });
