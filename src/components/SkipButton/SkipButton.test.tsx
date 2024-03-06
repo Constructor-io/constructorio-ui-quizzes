7b600add46c33f4c90b53237dc7d263c1885efba
vishnu <vishnu452@gmail.com>
2024-03-06 21:03:13 +0530

    husky pre commit, set up react testing library env

--- src/components/SkipButton/SkipButton.test.tsx
+++ src/components/SkipButton/SkipButton.test.tsx
@@ -8,14 +8,18 @@ describe(SkipButton, () => {
     propsGetters: () => ({}),
   };
 
-  it('renders a button with the default text', () => {
-    render(<SkipButton {...props} />);
+  it('renders a button with the default properties', () => {
+    const { container } = render(<SkipButton {...props} />);
     expect(screen.getByText('Skip')).toBeInTheDocument();
+    expect(container.firstChild).toHaveClass('cio-button-container');
   });
 
-  it("renders a button with the text 'Skip'", () => {
-    render(<SkipButton {...props} skipQuestionButtonText='Custom Button Text' />);
+  it('renders a button with custom properties', () => {
+    const { container } = render(
+      <SkipButton {...props} skipQuestionButtonText='Custom Button Text' className='custom-class' />
+    );
     expect(screen.getByText('Custom Button Text')).toBeInTheDocument();
     expect(screen.queryByText('Skip')).not.toBeInTheDocument();
+    expect(container.firstChild).toHaveClass('custom-class');
   });
 });
