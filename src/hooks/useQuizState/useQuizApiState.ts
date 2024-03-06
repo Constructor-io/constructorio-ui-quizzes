e13ec7e37509acd47210137d5ef4a0e6f17fb4e7
Islam Moustafa <58053149+mocca102@users.noreply.github.com>
2024-03-06 17:32:42 +0200

    Handle answers required for createQuizUrl function (#169)
    * Handle answers required for createQuizUrl function
    
    * call dispatchQuizResults only when answers array is present
    
    * getNextQuestion only when skipToResults is false

--- src/hooks/useQuizState/useQuizApiState.ts
+++ src/hooks/useQuizState/useQuizApiState.ts
@@ -110,9 +110,9 @@ const useQuizApiState: UseQuizApiState = (
       });
       if (isSharedResultsQuery) {
         await dispatchSharedQuizResults();
-      } else if (skipToResults) {
+      } else if (skipToResults && quizLocalState.answers.length) {
         await dispatchQuizResults();
-      } else {
+      } else if (!skipToResults) {
         try {
           const quizVersionId = quizLocalState.quizVersionId || quizVersionIdProp;
           const { quizSessionId } = quizLocalState;
