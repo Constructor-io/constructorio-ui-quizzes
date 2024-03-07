d4ee1905b6eee8d21815a55f2716817ea09ce939
vishnu <vishnu452@gmail.com>
2024-03-07 21:16:17 +0530

    added server test, rule links, client and server tests

---
+++ src/components/SkipButton/SkipButton.server.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import SkipButton from './SkipButton';
+
+describe(SkipButton, () => {
+  const props: React.ComponentProps<typeof SkipButton> = {
+    propsGetters: () => ({}),
+    skipQuestionButtonText: 'Custom Button Text',
+    className: 'custom-class',
+  };
+
+  it('renders a button with the default properties', () => {
+    const view = renderToString(<SkipButton propsGetters={() => ({})} />);
+    expect(view).toContain('Skip');
+    expect(view).toContain('cio-button-container');
+  });
+
+  it('renders a button with custom properties', () => {
+    const view = renderToString(<SkipButton {...props} />);
+    expect(view).toContain('Custom Button Text');
+    expect(view).toContain('custom-class');
+  });
+
+  it('does not render button without propsGetters', () => {
+    const view = renderToString(<SkipButton />);
+    expect(view).not.toContain('Skip');
+  });
+});
