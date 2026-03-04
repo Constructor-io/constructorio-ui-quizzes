1f8f1d473aeb4b26ce879cab2b11083e185e9deb
Tarek Ahmed <tarek.ahmed@tarek.ahmed-F5P62C970K>
2026-03-04 20:00:53 +0200

    Update storybook imports

--- src/stories/Quiz/Component/ErrorAndZeroResults.stories.tsx
+++ src/stories/Quiz/Component/ErrorAndZeroResults.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import CioQuiz from '../../../components/CioQuiz';
 import { quizId } from '../../../constants';
 import { callbacks } from '../tests/mocks';
--- src/stories/Quiz/Component/QuizResults/ZeroResults.stories.tsx
+++ src/stories/Quiz/Component/QuizResults/ZeroResults.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import React from 'react';
 import ZeroResults from '../../../../components/ZeroResults/ZeroResults';
 import { ZeroResultsDecorator } from './QuizResultsDecorator';
